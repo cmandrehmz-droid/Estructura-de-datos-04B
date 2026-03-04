cd7d470212e93f3ced9954d461716b505cc36472
AndrehMZ <cmandrehmz@gmail.com>
2026-03-04 07:18:28 -0500

    update.md

--- Ejercicios- Estructura de datos/Arrays-y-Busqueda/src/Ejercicio_5.java
+++ Ejercicios- Estructura de datos/Arrays-y-Busqueda/src/Ejercicio_5.java
@@ -1,4 +1,8 @@
 public class Ejercicio_5 {
+   /*  5. Contador de Estudiantes (Registro Académico)
+    Un profesor tiene un arreglo con las notas finales de 30 estudiantes (ej: 3.5, 4.0, 2.8, 5.0, ...).
+    El Problema: El profesor necesita saber exactamente cuántos estudiantes sacaron una nota de 5.0.
+    Algoritmo a usar: Búsqueda Lineal con Contador. Debes recorrer todo el arreglo y, cada vez que encuentres un 5.0, aumentar una variable contador. */
     public static void main(String[] args) {
         double[] notas = {
             3.5, 4.0, 2.8, 5.0, 4.5, 5.0, 3.2, 4.8, 2.9, 5.0,3.7, 4.1, 3.0, 4.9, 5.0,
