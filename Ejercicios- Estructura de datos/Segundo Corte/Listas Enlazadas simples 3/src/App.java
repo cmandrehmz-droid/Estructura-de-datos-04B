669599d26e540399422f11ad06160d5fb2318c41
AndrehMZ <cmandrehmz@gmail.com>
2026-04-01 07:01:01 -0500

    update.md

--- Ejercicios- Estructura de datos/Segundo Corte/Listas Enlazadas simples 3/src/App.java
+++ Ejercicios- Estructura de datos/Segundo Corte/Listas Enlazadas simples 3/src/App.java
@@ -1,3 +1,8 @@
+/* 3. Torre de Control (Aterrizajes de Emergencia)
+Simula la cola de aviones esperando para aterrizar en un aeropuerto congestionado.
+La Clase Vuelo (Nodo): Debe contener numeroVuelo (String), aerolinea (String), combustibleRestante (int) y pasajeros (int).
+El Problema: Normalmente los vuelos se forman al final de la cola. Sin embargo, si un vuelo reporta menos de 10 unidades de combustible, debe ser movido inmediatamente al inicio de la lista (Cabeza).
+Reto: Implementar el método reportarEmergencia(String numeroVuelo) que busque un vuelo en la cola y lo mueva al principio. */
 import java.util.Scanner;
 
 public class App {
